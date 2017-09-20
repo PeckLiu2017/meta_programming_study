c2f91f82f3c79ad66d226a3f95b1c2167ea0f906
peck liu <xiaoyang20170204@gmail.com>
2017-09-20 21:58:16 +0800

    practice4

--- chapter4/blocks/block_local_vars_failure.rb
+++ chapter4/blocks/block_local_vars_failure.rb
@@ -7,10 +7,9 @@ top_level_variable = 1
 just_yield do
   top_level_variable += 1
   local_to_block = 1
-  puts local_to_block
+  puts local_to_block # => 1
 end
 
-# => 1
 puts top_level_variable # => 2
 puts local_to_block
 # => block_local_vars_failure.rb:24:in `<main>':
